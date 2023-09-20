344bdae5a4a27a89b03c95931d0797e1730ab11a
Winny <winny.chelangat2@student.moringaschool.com>
2023-09-20 22:12:50 +0300

    Completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,14 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+        response= requests.get(url)
+        return response.content
+
 
     def load_json(self):
-        pass
+        programs = json.loads(self.get_response_body())
+        return programs
+    
+    data = GetRequester.load_json
+    print(data)
